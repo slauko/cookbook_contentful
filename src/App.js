7191b61ef61748e1a319fe3db564c886e572f428
slauko <slauko94@gmail.com>
2022-03-08 17:56:05 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {createClient} from "contentful"
+
+const client = createClient({
+  space: process.env.REACT_APP_SPACE_ID,
+  accessToken: process.env.REACT_APP_AUTH_TOKEN,
+
+});
+
 function App() {
+  console.log(process.env)
+
   return (
     <div className="App">
       Test
+      {console.log(client.getEntries({
+        content_type: "recipe",
+      }))}
     </div>
   );
 }
