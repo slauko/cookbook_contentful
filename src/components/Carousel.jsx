2134b18c1824c513f64be164c3f85af3d1feb979
Thomas <schmiddla@gmail.com>
2022-03-09 12:25:10 +0100

    bootstrap tests

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,32 @@
+// import Alert from 'bootstrap/Alert';
+// import { Carousel } from 'bootstrap';
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import Carousel from 'react-bootstrap/Carousel';
 
-export default function Carousel() {
-  return <div>Carousel</div>;
+export default function RecipeCarousel() {
+  return (
+    <div>
+      <Button variant="primary">Primary</Button>
+      <Carousel>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.oetker.de/Recipe/Recipes/oetker.de/de-de/baking/image-thumb__114338__RecipeDetailsLightBox/bienen-kuchen.webp"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+      <Alert dismissible variant="danger">
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+        <p>Change this and that and try again.</p>
+      </Alert>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
