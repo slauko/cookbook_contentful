3abcf4d77ed35b48902b0e52c1c0c2de2ecf3f7b
Haukman99 <h.nierling@osnanet.de>
2022-03-09 11:07:21 +0100

    Add RecipeClass

--- src/App.js
+++ src/App.js
@@ -4,15 +4,44 @@ import Footer from './views/Footer';
 import Nav from './views/Nav';
 import Landing from './views/Landing';
 import SearchResults from './views/SearchResults';
+import React, { useEffect } from 'react';
 import Recipe from './views/Recipe';
+import RecipeClass from './classes/RecipeClass';
 import './app.css';
 
 const client = createClient({
-  space: process.env.REACT_APP_SPACE_ID,
-  accessToken: process.env.REACT_APP_AUTH_TOKEN,
+  space: "h5pc0o4cj51n",
+  accessToken: "oijppwp2beVledF05dFDxlayGoNexONvWIqiizuINnw",
 });
 
 function App() {
+  useEffect(() => {
+    fetchData(client);
+  }, []);
+
+  function fetchData(client) {
+    let results = client.getEntries({
+      content_type: "recipe",
+    }).then(function (entries) {
+      populate(entries);
+    });
+    console.log(results);
+  }
+
+  const populate = (u) => {
+    let a = [];
+
+    u.items.forEach((hit) => {
+      if (!hit.fields.title) return;
+
+      const article = new RecipeClass(hit.fields);
+
+      a.push(article);
+    });
+
+    console.log(a)
+  };
+
   console.log(process.env);
 
   return (
